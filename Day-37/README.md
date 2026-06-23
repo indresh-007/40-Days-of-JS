eb331e049c94d1f5ef2340d89e25ef0448020bd3
indresh-007 <indresh209721@gmail.com>
2026-06-23 21:04:08 +0530

    Day 37 - JavaScript Web APIs

---
+++ Day-37/README.md
@@ -0,0 +1,13 @@
+Day 37 - JavaScript Web APIs
+
+🎯 Goal of This Lesson
+
+1. What are Web APIs?
+2. How Web APIs Work?
+3. Web APIS Marketplace
+4. Copy to Clipboard
+5. Local Storage
+6. Session Storage
+7. Geolocation
+8. Build It
+9. Open Source Web API Playground
---
+++ Day-37/index.js
@@ -0,0 +1,23 @@
+// - What I learning today?
+
+// 1. What are Web APIs?
+// 2. How Web APIs Work?
+// 3. Core Web APIs
+/*
+    a. DOM
+    b. Events
+    c. Timer
+    d. Fetch
+    e. Console
+    f. Copy
+    g. Storage
+    h. Geolocation
+    i. Notification
+    j. EyeDropper
+*/
+
+// 1. What are Web APIs?
+
+/*
+A Web API in JavaScript is a browser-provided interface that lets your JavaScript code interact with features outside the core language — like the DOM, network, clipboard, camera, etc.
+*/
